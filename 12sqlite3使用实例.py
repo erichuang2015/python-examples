eddbf51eb2355941f4426da144ec7fe1c17ee84a
zzzzer <zzzzer91@gmail.com>
2017-08-26 15:15:59 +0800

    update

--- 12sqlite3使用实例.py
+++ 12sqlite3使用实例.py
@@ -5,9 +5,9 @@ import sqlite3
 
 
 # 连接当前目录下的数据库, 不存在则创建
-conn = sqlite3.connect('example.db')
+db = sqlite3.connect('example.db')
 # 创建一个游标
-cursor = conn.cursor()
+cursor = db.cursor()
 # 创建表
 cursor.execute('create table info ('\
     'id int primary key,'\
@@ -22,9 +22,9 @@ cursor.execute('select id, name, age, sex from info where id = 1')
 lst = cursor.fetchall()
 print(lst)
 
-# 提交事务(不提交, 任何修改数据库的行为都不会生效)
-conn.commit()
 # 关闭游标
 cursor.close()
+# 提交事务(不提交, 任何修改数据库的行为都不会生效)
+db.commit()
 # 关闭连接
-conn.close()
+db.close()
---
+++ 25pymysql使用实例.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+r'''和sqlite3用法基本一致'''
+
+import pymysql
+
+
+# 不指定主机和端口, 默认本机
+# 需要指定utf8编码, 不然显示不出中文
+db = pymysql.connect(user='root', passwd='123', db='people', charset='utf8')
+# 创建一个游标
+cursor = db.cursor()
+# 执行一条语句
+cursor.execute("select * from info")
+# 返回选择的信息, 类型是tuple构成的列表
+lst = cursor.fetchall()
+print(lst)
+
+# 关闭游标
+cursor.close()
+# 提交事务(不提交, 任何修改数据库的行为都不会生效)
+db.commit()
+# 关闭连接
+db.close()
