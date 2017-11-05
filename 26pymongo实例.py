c9f8aa8717f2ab494b22a3a5118d220a32d8aed1
zzzzer <zzzzer91@gmail.com>
2017-11-05 16:22:11 +0800

    update

--- 26pymongo实例.py
+++ 26pymongo实例.py
@@ -4,13 +4,17 @@
 import pymongo
 
 
-with pymongo.MongoClient() as ct:
-    db = ct['temp']
+with pymongo.MongoClient() as conn:
+    db = conn['temp']
     tb = db['temp']
-    # 插入数据
-    tb.insert_one({'name': 'nihao'})
-    # 插入item, 若id已存在, 则放弃插入
+
     item = {'id':123, 'name': 'nihao'}
+
+    # 插入数据
+    tb.insert_one(item)
+
+    # 插入item, 若id已存在, 则放弃插入
     tb.update_one({'id': item['id']}, {'$set': item}, True)
+
     # 读取数据
     lst = list(tb.find(projection={"_id":0})) # 通过projection参数过滤掉指定键, 0为不显示, 1为显示
