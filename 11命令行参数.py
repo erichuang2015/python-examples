cd212035e4002898dcd1d3259e7b18dbcbd71ab9
zzzzer <zzzzer91@gmail.com>
2018-05-10 23:15:12 +0800

    update

--- 11命令行参数.py
+++ 11命令行参数.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 
-"""第一种方法"""
+"""第一种方法."""
 # import sys
 # if len(sys.argv) >= 2:
 #     print(sys.argv[1])
@@ -10,40 +10,101 @@
 #     print("请输入单词")
 
 
-"""第二种方法"""
+"""第二种方法."""
 import argparse
 
-# parser = argparse.ArgumentParser(description='Process some integers.')
-# parser.add_argument('integers', metavar='N', type=int, nargs='+',
-#                    help='an integer for the accumulator')
-# parser.add_argument('--sum', dest='accumulate', action='store_const',
-#                    const=sum, default=max,
-#                    help='sum the integers (default: find the max)')
 
-# args = parser.parse_args()
-# print(args.accumulate(args.integers))
+def arg_parse():
+    """模拟mysql命令行参数."""
+
+    parser = argparse.ArgumentParser(description='A Python-MySQL client')
+    
+    # add_argument()各个参数含义
+    # name/flags: 参数的名字
+    # action: 遇到参数时的动作, 默认值是store;
+    #         store_true, 表示那种没有值的额外参数;
+    #         store_const, 表示赋值为const;
+    #         append, 将遇到的值存储成列表, 也就是如果参数重复则会保存多个值;
+    #         append_const, 将参数规范中定义的一个值保存到一个列表;
+    #         count, 存储遇到的次数; 此外, 也可以继承argparse.Action自定义参数解析;
+    # nargs: 参数的个数, 可以是具体的数字;
+    #        或'+', 代表1个或多个参数;
+    #        或'*', 代表0个或多个参数;
+    #        或'?', 代表0个或一个参数.
+    # choices: 参数可允许的值的一个列表
+    # default: 不指定参数时的默认值
+    # type: 参数的类型
+    # choices: 参数允许的值
+    # required: 参数是否可以被省略
+    # help: 参数的帮助信息
+    # dest: 解析后参数的变量名, 不指定时是参数的名称
+    # metavar: 在usage说明中, 参数的名称, 不指定时是dest的内容大写
+
+    # 不加'-'或'--', 代表这是个不需要指定参数的值
+    # 此时不要指定dest参数的值, 因为'host'默认赋给了dest
+    parser.add_argument(
+        'host',
+        metavar='HOST',
+        help='connect to host'
+    )
+    parser.add_argument(
+        '-u',
+        '--user',
+        dest='user', # dest其实可以不指定
+        required=True,
+        help='user for login'
+    )
+    parser.add_argument(
+        '-p',
+        '--password',
+        dest='password',
+        required=True,
+        help='password to use when connecting to server'
+    )
+    parser.add_argument(
+        '-P',
+        '--port',
+        dest='port',
+        default='3306',
+        type=int,
+        help='port number to use for connection or 3306 or default'
+    )
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='%(prog)s 0.1'
+    )
+    # 实现那种-v输出详细内容, -vv输出更详细内容的功能
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='count',
+        default=0, # 这里的default不能不指定, 否则是None
+        help='increase output verbosity'
+    )
+    # 实现不需要指定值的参数功能
+    # 不指定时, 该值默认是False, 所以不用再设置default参数
+    parser.add_argument(
+        '-d',
+        '--daemon',
+        action='store_true',
+        help='whether daemon'
+    )
+    return parser.parse_args()
 
 
-parser = argparse.ArgumentParser(
-        description='Use TUNA mirrors everywhere when applicable')
-parser.add_argument(
-    'subcommand',
-    nargs='?',
-    metavar='SUBCOMMAND',
-    choices=['up', 'down', 'status'],
-    default='up')
-parser.add_argument(
-    '-v', '--verbose', help='verbose output', action='store_true')
-parser.add_argument(
-    '-y',
-    '--yes',
-    help='always answer yes to questions',
-    action='store_true')
-parser.add_argument(
-    '-g',
-    '--global',
-    dest='is_global',
-    help='apply system-wide changes. This option may affect applicability of some modules.',
-    action='store_true')
+def main():
+    parser = arg_parse()
+    conn_args = dict(
+        host=parser.host,
+        user=parser.user,
+        password=parser.password,
+        port=parser.port
+    )
+    print(conn_args)
+    print(parser.verbose)
+    print(parser.daemon)
 
-args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
---
+++ 28logging模块用法.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""logging模块用法"""
+
+import logging
+
+
+def base():
+    """logging模块的基本使用."""
+
+    # 指定 filename 参数, 可将日志输出到指定文件, 同时屏幕上将不输出
+    # format 参数可指定输出格式
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+    s = '0'
+    n = int(s)
+    logging.info('n = %d' % n)
+    print(10 / n)
+
+
+def advance():
+    """logging模块高级用法.
+    
+    既要把日志输出到控制台, 还要指定某个等级写入日志文件, 就要用这种方法.
+    """
+
+    # 第一步, 创建一个logger
+    logger_name = 'test_log'
+    logger = logging.getLogger(logger_name)  
+    logger.setLevel(logging.INFO)   # Log等级总开关, 即当每个handler不指定level时, 默认采用
+    
+    # 第二步, 创建一个handler, 用于输出到控制台
+    cmd_handler = logging.StreamHandler()  
+    #cmd_handler.setLevel(logging.INFO)   # 不指定level时, 默认采用logger的level 
+    
+    # 第三步, 再创建一个handler, 用于写入日志文件
+    log_file = 'logger.log'  
+    file_handler = logging.FileHandler(log_file, mode='w') # 'w' 模式每次都会覆盖, 'a' 模式追加
+    file_handler.setLevel(logging.WARNING)   # 输出到file的log等级的开关  
+    
+    # 第四步, 定义handler的输出格式
+    formatter = logging.Formatter(
+        '[%(asctime)s] - %(name)s - %(filename)s:%(lineno)d - %(levelname)-8s: %(message)s'
+    ) 
+    file_handler.setFormatter(formatter)  
+    cmd_handler.setFormatter(formatter)
+    
+    # 第五步, 将logger添加到handler里面  
+    logger.addHandler(file_handler)  
+    logger.addHandler(cmd_handler)
+    
+    # 测试, 级别从低到高
+    logger.debug('this is a logger debug message')  
+    logger.info('this is a logger info message')  
+    logger.warning('this is a logger warning message')  
+    logger.error('this is a logger error message')  
+    logger.critical('this is a logger critical message')
+
+
+if __name__ == '__main__':
+    advance()
