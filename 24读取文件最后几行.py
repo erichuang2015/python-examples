c8890702c9cc1b3fe2da1231da9c1dc5710adfa2
zzzzer <zzzzer91@gmail.com>
2018-05-14 21:40:38 +0800

    update

---
+++ 24读取文件最后几行.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""用deque实现读取文件最后n行."""
+
+from collections import deque
+from collections import Iterator, Iterable
+
+
+if __name__ == '__main__':
+    with open('00test.py') as f:
+        # deque第一个参数接收一个可迭代对象
+        last_n_lines = deque(f, maxlen=5)
+    for line in last_n_lines:
+        # line中已有换行符, 所以把print自带的换行符去掉
+        print(line, end='')
+    # 可以被 for 作用的称为可迭代对象, Iterable
+    print(isinstance(last_n_lines, Iterable))
+    # 可以被 next() 函数调用并不断返回下一个值的对象称为迭代器, Iterator
+    print(isinstance(last_n_lines, Iterator))
--- desginpatterns/01单例模式.py
+++ desginpatterns/01单例模式.py
@@ -3,6 +3,7 @@
 
 
 class Singleton:
+    """经典单例模式."""
 
     def __new__(cls, *args, **kw):
         if not hasattr(cls, '_instance'):
@@ -10,9 +11,30 @@ class Singleton:
             # 所有类都默认继承自object
             # __new__中传入的cls参数不能省略
             # 但__init__中传入的self参数却不能传入
-            cls._instance = super(Singleton, cls).__new__(cls, *args, **kw)
+            cls._instance = super(Singleton, cls).__new__(cls, *args, **kw))
             # python3中, super()参数可以省略
             #cls.instance = super().__new__(cls, *args, **kw)
+        return cls._instance
+
+
+class Singleton2:
+    """单例模式中的懒汉式.
+    
+    仅在需要时才创建.
+    """
+
+    _instance = None
+    
+    def __init__(self):
+        if not self._instance:
+            print('__init__ method called...')
+        else:
+            print('Instance already created:', self.get_instance())
+    
+    @classmethod
+    def get_instance(cls):
+        if not cls._instance:
+            cls._instance = Singleton2()
         return cls._instance
 
 
@@ -21,6 +43,12 @@ def main():
     print(s1)
     s2 = Singleton()
     print(s2)
+    print('='*50)
+    s1 = Singleton2()
+    print('%x' % id(Singleton2))
+    print('%x' % id(s1))
+    print('Object created', Singleton2.get_instance())
+    s2 = Singleton2()
 
 
 if __name__ == '__main__':
