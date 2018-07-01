03f093e474225865a587bb1d117b1732f5b5f5c5
zzzzer <zzzzer91@gmail.com>
2018-07-01 15:40:36 +0800

    update

--- 24读取文件最后几行.py
+++ 24读取文件最后几行.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
         # deque第一个参数接收一个可迭代对象
         # 只保留最后5行
         last_n_lines = deque(f, maxlen=5)
-    for line in last_n_lines:
-        # line中已有换行符, 所以把print自带的换行符去掉
-        print(line, end='')
     # 可以被 for 作用的称为可迭代对象, Iterable
     print(isinstance(last_n_lines, Iterable))
     # 可以被 next() 函数调用并不断返回下一个值的对象称为迭代器, Iterator
     print(isinstance(last_n_lines, Iterator))
+    for line in last_n_lines:
+        # line中已有换行符, 所以把print自带的换行符去掉
+        print(line, end='')
