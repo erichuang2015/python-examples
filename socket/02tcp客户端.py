6c4fdb846d40bbaadf6738adb43b7a429f1432a7
zzzzer <zzzzer91@gmail.com>
2018-08-20 10:53:31 +0800

    update

--- socket/02tcp客户端.py
+++ socket/02tcp客户端.py
@@ -3,7 +3,7 @@
 
 """简单tcp客户端.
 
-发送字符串给目标服务器, 并接受服务器返回的内容.
+发送字符串给目标服务器，并接受服务器返回的内容。
 """
 
 import socket
@@ -15,27 +15,27 @@ def main():
     # AF_INET: 使用IPv4地址和端口号
     # SOCK_STREAM: 面向连接, 即tcp
     # SOCK_DGRAM: 面向非连接, 即udp
-    cfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # 连接服务, 设置连接的主机和端口号（端口号只能用int类型）
     address = ('127.0.0.1', 9527)
     # tcp相比较udp需要先于与目标服务器建立连接
     print('连接%s:%d中...' % address, end='')
-    cfd.connect(address)
+    client_sock.connect(address)
     print('成功!')
 
     while True:
         try:
             send_data = input()
-        except EOFError:  # 监测C+d
+        except (EOFError, KeyboardInterrupt):  # 监测 C+d 和 C+c
             break
-        cfd.sendall(send_data.encode('utf-8'))
-        recv_data = cfd.recv(BUF_SIZE)
+        client_sock.sendall(send_data.encode('utf-8'))
+        recv_data = client_sock.recv(BUF_SIZE)
         if not recv_data:
             print('对方服务器', address, '已关闭')
             break
         print(recv_data.decode('utf-8'))
-    cfd.close()
+    client_sock.close()
 
 
 if __name__ == '__main__':
--- socket/07epoll.py
+++ socket/07epoll.py
@@ -1,82 +1,142 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-"""
-LT模式: epoll默认模式, 只要缓冲区还有数据就会触发事件.
-ET模式: 事件只触发第一次, 无论缓冲区是否还有数据, 所以有可能导致数据被丢弃.
+"""epoll 使用实例。
 
-详见下面例子.
+事件类型：
+　　select.EPOLLIN    可读事件
+　　select.EPOLLOUT   可写事件
+　　select.EPOLLERR   错误事件
+　　select.EPOLLRDHUP 客户端断开事件
+　　select.EPOLLHUP   「which signals an unexpected close of the socket, i.e. usually an internal error」，
+                     这个事件是不需要专门注册的，默认就有，详见 `man epoll_ctl` 。
+
+LT模式：epoll 默认模式，只要缓冲区还有数据就会触发事件。
+ET模式：事件只触发第一次，无论缓冲区是否还有数据，
+       所以一旦触发，需要循环把缓冲区数据读完，
+       这要求将 socket 设为非阻塞。
+
+注意点：
+当客户端连接本服务器时，会触发 select.EPOLLIN 。
+当客户端有数据发送过来时，会触发 select.EPOLLIN 。
+当对应 socket 发送缓冲区未满时，会触发 select.EPOLLOUT 。
+当客户端关闭时，会触发 select.EPOLLIN | select.EPOLLRDHUP 。
 """
 
 import socket
 import select
 
 
-def main():
-    # 创建epoll
-    epoll = select.epoll()
+class Manager:
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # 端口复用
-    server_socket.bind(('localhost', 9527))
-    server_socket.listen(4)
-    server_socket.setblocking(False)  # 服务端设置非阻塞
+    def __init__(self, address, max_events):
 
-    # 注册监听server_socket
-    # 第一个参数是：要监听的socket的文件描述符
-    # 第二参数：要监听的事件类型
-    # ET模式：当epoll检测到描述符事件发生并将此事件通知应用程序, 应用程序必须立即处理该事件。下次将不会在通知。
-    epoll.register(server_socket.fileno(), select.EPOLLIN | select.EPOLLET)
-    # 文件句柄到所对应对象的字典，格式为{句柄：对象}
-    socket_dict = {}
+        self.address = address
+        self.max_events = max_events
 
-    try:
+        # 创建epoll
+        self.epoll = select.epoll()
+
+        # 创建服务套接字
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 服务端设置非阻塞
+        self.server_socket.setblocking(False)
+        # 端口复用
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind(self.address)
+        self.server_socket.listen(self.max_events)
+        self.server_fd = self.server_socket.fileno()
+
+        # 注册监听server_socket
+        # 第一个参数是：要监听的socket的文件描述符
+        # 第二参数：要监听的事件类型，这里设置 ET 模式
+        self.epoll.register(self.server_socket.fileno(), select.EPOLLIN | select.EPOLLET)
+
+        # 文件句柄到所对应对象的字典，格式为{句柄：对象}
+        self.fd_to_socket = {}
+        # 缓存发送内容
+        self.fd_to_buffer = {}
+
+    def run(self):
         while True:
             print('等待事件中...')
-            # 如果有新的客户端和可以收数据的socket和断开的socket就解除阻塞, 并且返回列表
-            epoll_list = epoll.poll()
-            # [(3, 1)] 3是文件描述符, 1是有数据进来了
-            print(epoll_list)
+            events = self.epoll.poll(maxevents=self.max_events)
 
-            # 轮询注册的事件集合, 返回值为[(文件句柄, 对应的事件), (...), ...]
-            for fd, event in epoll_list:
-                # 当有客户端连接的时候
-                if fd == server_socket.fileno():
-                    # 有新的客户端链接了
-                    client_socket, client_address = server_socket.accept()
-                    # 客户端也设置不阻塞
-                    client_socket.setblocking(False)
-                    # 当有新的链接就会创建新的sockect，也要注册到epoll中
-                    epoll.register(client_socket.fileno(), select.EPOLLIN | select.EPOLLET)
-                    # 加入字典, 用于读时取出文件描述符对应的套接字对象
-                    socket_dict[client_socket.fileno()] = client_socket
-                    print('已被连接:', client_address)
+            # 轮询注册的事件集合, 返回格式为[(文件句柄, 对应的事件), ...]
+            for fd, event in events:
+                print('fd: {}, event: {}'.format(fd, event))
+
+                # 当有客户端连接的时候，会触发 EPOLLIN 事件
+                if fd == self.server_fd:
+                    self.accept_handler()
+
                 # 当有客户端的数据发送过来的时候，会触发 EPOLLIN 事件
-                else:
-                    client_socket = socket_dict[fd]
-                    buf = b''
-                    try:
-                        while True:  # 在 ET 模式下，需要循环读，直到缓冲区无数据
-                            print('非阻塞读')
-                            # 将接收缓冲区设的很小,
-                            # 用于测试LT和ET模式
-                            recv_data = client_socket.recv(4)
-                            if not recv_data:
-                                print('对端关闭:', client_socket.getpeername())
-                                client_socket.close()
-                                epoll.unregister(fd)
-                                socket_dict.pop(fd)
-                            buf += recv_data
-                    except BlockingIOError:  # socket 设为非阻塞，缓存区无数据时读，会抛出这个异常
-                            print(buf.decode())
-                            client_socket.send(buf.upper())
+                elif event & select.EPOLLIN:
+                    self.read_handler(fd)
+
+                # 当网络发送缓冲区未满，可以发送时，会触发 EPOLLOUT 事件
+                elif event & select.EPOLLOUT:
+                    self.write_handler(fd)
+
+    def accept_handler(self):
+        # 有新的客户端链接了
+        client_socket, client_address = self.server_socket.accept()
+        # 客户端也设置不阻塞
+        client_socket.setblocking(False)
+        # 当有新的链接就会创建新的sockect，也要注册到epoll中
+        self.epoll.register(client_socket.fileno(), select.EPOLLIN | select.EPOLLET)
+        # 加入字典，用于读时取出文件描述符对应的套接字对象
+        self.fd_to_socket[client_socket.fileno()] = client_socket
+        # 加入字典，用于读时取出文件描述符对应的缓冲区
+        self.fd_to_buffer[client_socket.fileno()] = None
+
+        print('已被连接:', client_address)
+
+    def read_handler(self, fd):
+        client_socket = self.fd_to_socket[fd]
+        buffer = b''
+        try:
+            while True:  # 在 ET 模式下，需要循环读，直到缓冲区无数据
+                # 将接收缓冲区设的很小,
+                # 用于测试LT和ET模式
+                recv_data = client_socket.recv(4)
+                if not recv_data:
+                    # 获取套接字对应地址
+                    print('对端关闭:', client_socket.getpeername())
+                    client_socket.close()
+                    self.epoll.unregister(fd)
+                    self.fd_to_socket.pop(fd)
+                    self.fd_to_buffer.pop(fd)
+                    break
+                buffer += recv_data
+        except BlockingIOError:  # socket 设为非阻塞，缓存区无数据时读，会抛出这个异常
+            self.epoll.modify(fd, select.EPOLLOUT | select.EPOLLET)
+            self.fd_to_buffer[fd] = buffer
+
+    def write_handler(self, fd):
+        client_socket = self.fd_to_socket[fd]
+        buffer = self.fd_to_buffer[fd]
+        print(buffer.decode())
+        client_socket.sendall(buffer.upper())
+        self.epoll.modify(fd, select.EPOLLIN | select.EPOLLET)
+
+    def close(self):
+        for client_socket in self.fd_to_socket.values():
+            client_socket.close()
+        self.server_socket.close()
+        self.epoll.close()
+
+
+def main():
+    address = ('localhost', 9527)
+    max_events = 2  # 这个参数其实不关键，对 `listen` 和 `epoll` 基本没影响
+    m = Manager(address, max_events)
+    try:
+        m.run()
     except KeyboardInterrupt:
         pass
     finally:
-        for client_socket in socket_dict.values():
-            client_socket.close()
-        server_socket.close()
-        epoll.close()
+        m.close()
         print('关闭成功!')
 
 
