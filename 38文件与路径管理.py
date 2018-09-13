a0152f003ff6a5f9bd2ae4a32f2364ab2706ac94
zzzzer <zzzzer91@gmail.com>
2018-09-14 07:48:59 +0800

    update

--- 38文件与路径管理.py
+++ 38文件与路径管理.py
@@ -5,28 +5,33 @@ import os
 
 
 # 获取当前路径
-os.getcwd()
+print(os.getcwd())
 # 列出当前路径下所有文件
-os.listdir('.')
+print(os.listdir('.'))
 # 改变路径
 os.chdir('..')
+print(os.getcwd())
+print('*' * 50)
 
 """1.拆分路径"""
 # 拿到的是绝对路径
 path = __file__
+print(path)
 # 返回一个文件路径和文件名组成的二元组
-os.path.split(path)
+print(os.path.split(path))
 # 返回文件的路径
-os.path.dirname(path)
+print(os.path.dirname(path))
 # 返回文件的文件名
-os.path.basename(path)
+print(os.path.basename(path))
 # 返回一个除去文件拓展名的部分和拓展名组成的二元组
-os.path.splitext(path)
+print(os.path.splitext(path))
+print('*' * 50)
 
 """2.构建路径"""
 # 展开用户目录
-os.path.expanduser('~')
+print(os.path.expanduser('~'))
 # 得到文件的绝对路径
-os.path.abspath('.')
+print(os.path.abspath('.'))
 # 拼接路径, 可以跨平台
-os.path.join('~', 't', 'a.py')
+print(os.path.join('~', 't', 'a.py'))
+print('*' * 50)
